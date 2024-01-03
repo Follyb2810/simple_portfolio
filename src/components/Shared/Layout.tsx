6b6ce40b3b7b06a170cc7cd5dd85f4244049752e
follyb2810 <babatundeyusuffolorunsho@gmail.com>
2024-01-03 13:19:35 -0800

    adding changes

--- src/components/Shared/Layout.tsx
+++ src/components/Shared/Layout.tsx
@@ -6,6 +6,7 @@ const Layout = () => {
   return (
     <section className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-10  h-screen'>
       <SideBar />
+      
       <section className='col-span-1 md:col-span-2 lg:col-span-8  h-screen relative'>
         <Outlet />
       </section>
